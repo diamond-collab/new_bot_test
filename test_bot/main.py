2f5d4bcabb6be9965ddaa78540698bb4a76adcc8
diamond-collab <lzaitsev6@gmail.com>
2025-05-04 18:41:03 +0500

    Update main.py

--- test_bot/main.py
+++ test_bot/main.py
@@ -2,12 +2,15 @@ import asyncio
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from handler import start  # импорт обработчиков
+from handler import help
+
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
 # подключаем обработчики
 start.register_handlers(dp)
+help.register_handlers(dp)
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
